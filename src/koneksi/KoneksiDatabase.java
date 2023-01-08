11245ed3c20cf168ad6bd7dc8a98c948374c75aa
Arsyadwidiatirtaarakian <117973072+Arsyadwidiatirtaarakian@users.noreply.github.com>
2023-01-08 11:30:51 +0700

    Add files via upload

---
+++ src/koneksi/KoneksiDatabase.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package koneksi;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author fakhr
+ */
+public class KoneksiDatabase {
+        Connection cn;
+        public static Connection bukaKoneksi(){
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/databasemusic", "root","");
+                return cn;
+            }catch (Exception e){
+                JOptionPane.showMessageDialog(null, e);
+                return null;
+            
+            }
+        
+        }
+
+
+
+
+    
+}
